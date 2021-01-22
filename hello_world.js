a9737bfc5b4beee504cdcb05f6bc1947246a7437
Bryan <walsh.c.bryan@gmail.com>
2021-01-21 21:49:31 -0600

    did some reading, messed around, deleted some silly functions

--- hello_world.js
+++ hello_world.js
@@ -20,3 +20,20 @@ function divide(num1, num2){
     else {
     return (num1 / num2);
 }}
+
+let phrase = "Programming today is a race between software engineers striving to build bigger and better idiot-proof programs, and the Universe trying to produce bigger and better idiots. So far, the Universe is winning.";
+let splitphrase = new Array();
+
+function split() {
+splitphrase = phrase.split(" ");
+document.write(splitphrase.join(" <br> "));
+}
+
+
+
+function addTheNums([nums]){
+    var total = 0;
+    for(i = 0; i < nums.length; i++){
+    total += nums[i]; }
+    return total;
+}
