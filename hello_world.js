4799f8378c9ff2689787601d4675d07a5bac272c
Bryan <walsh.c.bryan@gmail.com>
2021-01-21 21:12:30 -0600

    simple maths, one day at a time :)

--- hello_world.js
+++ hello_world.js
@@ -1,3 +1,22 @@
 let test="hello world"
 
 console.log(test)
+
+function runit(string) {
+    console.log(string);
+}
+
+function add(num1, num2) {
+    return (num1 + num2);
+}
+
+function multiply(num1, num2){
+    return (num1 * num2);
+}
+
+function divide(num1, num2){
+    if (num2 == 0) {  
+        console.log("cannot divide by zero ya dingus") } 
+    else {
+    return (num1 / num2);
+}}
